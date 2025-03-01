9c4aefb87ae0446f8958e13dd404a26a6a708b80
Elina <elinaslvv@gmail.com>
2025-03-01 18:02:57 +0200

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,25 +1,9 @@
 import { Contact } from "../models/contact.js";
 
-export async function getAllContacts() {
-  return Contact.find();
-  // return {
-  //   status: 200,
-  //   message: "Successfully found contacts!",
-  //   data: contacts
-  // };
-}
+export const getAllContacts = async () => {
+  return await Contact.find();
+};
 
-export async function getContactById(id) {
-  return Contact.findById(id);
-  // if (!contact) {
-  //   return {
-  //     status: 404,
-  //     message: "Contact not found"
-  //   };
-  // }
-  // return {
-  //   status: 200,
-  //   message: `Successfully found contact with id ${id}!`,
-  //   data: contact
-  // };
-}
+export const getContactById = async (contactId) => {
+  return await Contact.findById(contactId);
+};
