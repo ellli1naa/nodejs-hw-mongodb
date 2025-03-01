a9fbab10f83b72e3d7f53a608fe7cd3fd57ec85e
Elina <elinaslvv@gmail.com>
2025-03-01 18:00:22 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,32 +5,72 @@ import pinoHttp from "pino-http";
 import { getAllContacts, getContactById } from "./services/contacts.js";
 import { getEnvVar } from "./utils/getEnvVar.js";
 
-const logger = pino();
+const PORT = Number(getEnvVar('PORT', '3000'));
 
 export function setupServer() {
   const app = express();
-  const PORT = getEnvVar("PORT");
-
+  
   app.use(cors());
-  app.use(pinoHttp({ logger }));
   app.use(express.json());
+  app.use(
+    pinoHttp({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello!',
+    });
+  });
 
   app.get("/contacts", async (req, res) => {
     const result = await getAllContacts();
-    res.status(result.status).json(result);
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts',
+      data: contacts,
+    });
   });
 
   app.get("/contacts/:contactId", async (req, res) => {
     const { contactId } = req.params;
     const result = await getContactById(contactId);
-    res.status(result.status).json(result);
+    if (!result) {
+        return res.status(404).json({
+          status: 404,
+          message: 'Contact not found',
+        });
+      }
+
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}`,
+        data: contact,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   });
 
-  app.use((req, res) => {
-    res.status(404).json({ message: "Not found" });
+  app.get('*', (req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: 'Unavailable',
+   });
+
+    app.use((err, req, res, next) => {
+      res.status(500).json({
+        status: 500,
+        message: 'Something went wrong',
+        error: err.message,
+      });
+    });
   });
 
   app.listen(PORT, () => {
-    logger.info(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
   });
 }
