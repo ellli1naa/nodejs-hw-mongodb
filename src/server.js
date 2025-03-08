d186b937a26862383c4e3d39cbc39a32ed51bfef
elina <elinaslvv@gmail.com>
2025-03-08 12:54:39 +0200

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
-import express from "express";
-import cors from "cors";
-import pinoHttp from "pino-http";
-import { getAllContacts, getContactById } from "./services/contacts.js";
-import { getEnvVar } from "./utils/getEnvVar.js";
+import express from 'express';
+import cors from 'cors';
+import pinoHttp from 'pino-http';
+import contactsRouter from './routers/contacts.js';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
 export function setupServer() {
   const app = express();
-  
+
   app.use(cors());
   app.use(express.json());
   app.use(
@@ -25,59 +27,11 @@ export function setupServer() {
     });
   });
 
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello!',
-    });
-  });
+  app.use(contactsRouter);
 
-  app.get('/contacts', async (req, res) => {
-    const result = await getAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts',
-      data: result,
-    });
-  });
+  app.get('*', notFoundHandler);
 
-  app.get("/contacts/:contactId", async (req, res) => {
-    try {
-      const { contactId } = req.params;
-      console.log('Received contactId:', contactId);
-
-      const contact = await getContactById(contactId);
-
-      if (!contact) {
-        return res.status(404).json({
-          status: 404,
-          message: 'Contact not found',
-        });
-      }
-
-      res.status(200).json({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}`,
-        data: contact,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  });
-
-  app.get('*', (req, res) => {
-    res.status(404).json({
-      status: 404,
-      message: 'Unavailable',
-   });
-
-    app.use((err, req, res, next) => {
-      res.status(500).json({
-        status: 500,
-        message: 'Something went wrong',
-        error: err.message,
-      });
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,3 +7,35 @@ export const getAllContacts = async () => {
 export const getContactById = async (contactId) => {
   return await Contact.findById(contactId);
 };
+
+export const createContact = async (payload) => {
+  const contact = await Contact.create(payload);
+  return contact;
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await Contact.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    student: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await Contact.findOneAndDelete({
+    _id: contactId,
+  });
+
+  return contact;
+};
